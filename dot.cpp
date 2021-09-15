d608f691f79a83bcfe68ac3bccd9fcde9341934f
mandarijn4 <75084929+mandarijn4@users.noreply.github.com>
2021-09-15 16:42:00 +0200

    dots
    Dots saved in vector of vectors of chars

--- dot.cpp
+++ dot.cpp
@@ -6,7 +6,6 @@ namespace st = std;
 
 int main() {
     auto image = st::vector <st::vector <char>> ();
-    st::cout << "test" << st::endl;
     for (auto imageIndex = 0; imageIndex < 51; imageIndex++)
     {
         st::vector<char>Column;
@@ -15,25 +14,7 @@ int main() {
         {
             image.at(imageIndex).push_back('.');  
         }
-    }
-
-    auto radius = 10;
-
-    auto centerX = (image.size() / 2);
-    auto centerY = (image[0].size() / 2);
-    image[centerX][centerY] = '0';
-
-    // auto yPostion = sqrt((radius * radius)+((centerX+5) * (centerX+5)));
-    // st::cout << yPostion << st::endl;
-    // image[centerX-5][yPostion] = '0';
-
-    for (auto xPosition = 0; xPosition <= radius; xPosition++)
-    {
-        auto yPosition = round(sqrt((radius * radius)-((xPosition-centerX) * (xPosition-centerX))));
-        st::cout << yPosition << st::endl;
-        image[xPosition + centerX][yPosition] = '0';
-    }
-           
+    }           
 
     for (auto imageI = 0; imageI < image.size(); imageI++)
     {
@@ -43,8 +24,5 @@ int main() {
         }
         st::cout << '\n';
     }
-
-    st::cout << centerX << st::endl;
-    st::cout << centerY << st::endl;
     return 0;   
 }
