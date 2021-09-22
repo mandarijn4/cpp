ab74cba8278960840c37b91d77c1f6aecffbeff3
mandarijn4 <75084929+mandarijn4@users.noreply.github.com>
2021-09-22 09:03:25 +0200

    update 22/9/2021 9:00

--- circle.cpp
+++ circle.cpp
@@ -19,8 +19,8 @@ int main() {
 
     auto sizeOfBoard = 101;
 
-    auto innerRadius = 10;
-    auto outerRadius = 20;
+    auto innerRadius = 20;
+    auto outerRadius = 40;
 
     auto playBoard = st::vector<st::vector<float>> ();
     
@@ -47,7 +47,7 @@ int main() {
             auto radius = sqrt(pow(x, 2) + pow(y, 2));
             if (radius >= innerRadius && radius <= outerRadius)
             {
-                playBoard[columnIndex][rowIndex] += 64;
+                playBoard[columnIndex][rowIndex] += 96;
             }
         }
     }
@@ -57,9 +57,10 @@ int main() {
         for (auto rowIndex = 0; rowIndex < sizeOfBoard; rowIndex++)
         {
             if (playBoard[columnIndex][rowIndex] == 0){st::cout << ' ' << ' ';}
-            else if (playBoard[columnIndex][rowIndex] == 32){st::cout << '.' << ' ';}
-            else if (playBoard[columnIndex][rowIndex] == 64){st::cout << ';' << ' ';}
-            else if (playBoard[columnIndex][rowIndex] == 96){st::cout << '=' << ' ';}
+            else if (playBoard[columnIndex][rowIndex] == 32){st::cout << '1' << ' ';}
+            else if (playBoard[columnIndex][rowIndex] == 64){st::cout << 'L' << ' ';}
+            else if (playBoard[columnIndex][rowIndex] == 96){st::cout << 'L' << ' ';}
+            else if (playBoard[columnIndex][rowIndex] == 128){st::cout << 'N' << ' ';}
             else {st::cout << '@' << ' ';}
         }
         st::cout << '\n';
--- lesson2/assignment/vector.cpp
+++ lesson2/assignment/vector.cpp
@@ -1,11 +1,64 @@
 #include <iostream>
 
+namespace st = std;
+
 struct Vec3D
 {
     float x,y,z;
 };
 
+Vec3D vec3D (float x, float y, float z){
+    struct Vec3D created;
+    created.x = x;
+    created.y = y;
+    created.z = z;
+    return created;
+}
+
+void show (){
+    st::cout << st::endl;
+}
+
+void show (st::string label, Vec3D const &self){
+    st::cout << "De x-coordinaat van de vector met de naam " << label << " is " << self.x << st::endl;
+    st::cout << "De y-coordinaat van de vector met de naam " << label << " is " << self.y << st::endl;
+    st::cout << "De z-coordinaat van de vector met de naam " << label << " is " << self.z << st::endl;
+    show();
+}
+
+void show (st::string label, float scalar){
+    st::cout << "De scalar behorend bij het label " << label << " is " << scalar << st::endl;
+    show();
+}
+
+Vec3D minus (Vec3D const &self){
+    return vec3D(-self.x, -self.y, -self.z);
+}
+
+Vec3D add (Vec3D const &self, Vec3D const &other){
+    return vec3D(self.x + other.x, self.y + other.y, self.z + other.z);
+}
+
+Vec3D sub (Vec3D const &self, Vec3D const &other){
+    return vec3D(self.x - other.x, self.y - other.y, self.z - other.z);
+}
+
+Vec3D mul (Vec3D const &self, float scalar){
+    return vec3D(self.x * scalar, self.y * scalar, self.z * scalar);
+}
 
 int main(){
-
+    struct Vec3D random = vec3D(3, 8, 4);
+    struct Vec3D other = vec3D(6, 2, 5);
+    show();
+    show("random", random);
+    show("alpha", 8);
+    struct Vec3D invert = minus(random);
+    show("inverted", invert);
+    struct Vec3D resultAdd = add(random, other);
+    show("result", resultAdd);
+    struct Vec3D resultSub = sub(random, other);
+    show("result", resultSub);  
+    struct Vec3D resultMul = mul(random, 8);
+    show("result", resultMul);      
 }
