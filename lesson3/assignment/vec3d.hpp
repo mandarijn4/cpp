fda00c2a3e14ad0d89d0c3ecc96df360146411c2
mandarijn4 <75084929+mandarijn4@users.noreply.github.com>
2021-10-11 09:18:41 +0200

    test

---
+++ lesson3/assignment/vec3d.hpp
@@ -0,0 +1,26 @@
+#ifndef VEC3D_HPP
+#define VEC3D_HPP
+#include <iostream>
+#include <cmath>
+
+namespace st = std;
+
+class Vec3D {
+    public:
+    float x,y,z;
+    Vec3D(float x, float y, float z);
+    void show();
+    // void show(st::string label);
+    void show(st::string label, float scalar);
+    Vec3D minus();
+    Vec3D add(Vec3D const &other);
+    Vec3D sub(Vec3D const &other);
+    Vec3D mul(float scalar);
+    Vec3D div(float scalar);
+    float norm();
+    Vec3D unit();
+    float dot(Vec3D const &other);
+    Vec3D cross(Vec3D const &other);
+};
+
+#endif
